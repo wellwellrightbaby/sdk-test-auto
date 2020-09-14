3e26a4d388501afa2b3bb17e525131acfa5e1ced
unknown <873687837@qq.com>
2020-09-14 16:16:13 +0800

    change folder

--- test/index.ts
+++ test/index.ts
@@ -1,5 +1,7 @@
 // 动态加载所有当前文件夹下所有-spec.ts结尾的模块, false:不包括下一层级
-const files = require.context('./', true, /-spec.tsx?$/);
+//const files = require.context('./', true, /-spec.tsx?$/);
+
+const files = require.context('./', true, /zego-express-?/);
 
 const keys = files.keys();
 console.log('keys:', keys);
